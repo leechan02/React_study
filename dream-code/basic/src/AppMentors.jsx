5fde04ce9a27795d8f9963643ebcf189ec83cb56
Eui Lee <lechan1317@gmail.com>
2024-07-18 22:33:41 +0900

    feat: array

---
+++ dream-code/basic/src/AppMentors.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./AppXY.css";
+
+export default function AppMentors() {
+  const [person, setPerson] = useState({
+    name: "euiclee",
+    title: "developer",
+    mentors: [
+      {
+        name: "taeypark",
+        title: "developer",
+      },
+      {
+        name: "hyeoan",
+        title: "developer",
+      },
+    ],
+  });
+
+  return (
+    <div>
+      <h1>
+        {person.name} is a {person.title}
+      </h1>
+      <p>{person.name} mentor is:</p>
+      <ul>
+        {person.mentors.map((mentor, index) => (
+          <li key={index}>
+            {mentor.name} ({mentor.title})
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          const prev = prompt(`who's mentor do you want to change?`);
+          const current = prompt(`what is the new mentor's name?`);
+        }}
+      >
+        Change mentor name
+      </button>
+    </div>
+  );
+}

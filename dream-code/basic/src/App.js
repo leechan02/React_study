eaab838117399c24ffcbf23dd763b731e53101f7
Eui Lee <lechan1317@gmail.com>
2024-07-18 14:59:52 +0900

    feat: basic syntax

--- dream-code/basic/src/App.js
+++ dream-code/basic/src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'euiclee';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>hello</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        <li>apple</li>
+        <li>banana</li>
+        <li>orange</li>
+      </ul>
+    </>
   );
 }
 

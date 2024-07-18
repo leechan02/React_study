ac4fa14571d533e1c37fac392a920d6a22793769
Eui Lee <lechan1317@gmail.com>
2024-07-18 15:56:25 +0900

    feat: onclick

--- dream-code/basic/src/AppProfile.jsx
+++ dream-code/basic/src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Profile from "./components/Profile";
 import Avatar from "./components/Avatar";
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    console.log("버튼이 클릭되었습니다.");
+  }
   return (
     <>
+      <button onClick={handleClick}>button</button>
       <Avatar
         image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
         isNew={true}

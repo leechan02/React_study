95881bd9fdb77865bc36a606294db52b80ebaeba
Eui Lee <lechan1317@gmail.com>
2024-07-18 16:59:31 +0900

    feat: key

--- dream-code/basic/src/components/Products.jsx
+++ dream-code/basic/src/components/Products.jsx
@@ -24,7 +24,10 @@ export default function Products() {
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            {product.name} - {product.price}
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
           </li>
         ))}
       </ul>

7efae11543512a9f59a461ad20acdccecb416e9b
Eui Lee <lechan1317@gmail.com>
2024-07-18 16:35:19 +0900

    feat:counter

--- dream-code/basic/src/components/Counter.jsx
+++ dream-code/basic/src/components/Counter.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ total, onClick }) {
   const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <span className="number">{count}</span>
+      <span className="number">{count}<span className="total">/{total}</span></span>
       <button
-        className="button" onClick={() => {
-        setCount((prev) => prev + 1);
-        setCount((prev) => prev + 1);
-      }}>Add +</button>
+        className="button"
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          onClick();
+        }}
+      >
+        Add +
+      </button>
     </div>
   );
 }

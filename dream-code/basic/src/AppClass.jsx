25682bbd28393a0026ad46b9bfdbc10228914334
Eui Lee <lechan1317@gmail.com>
2024-07-19 13:45:23 +0900

    feat: class

---
+++ dream-code/basic/src/AppClass.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Counter from "./basic/components/Counter";
+
+export default class AppClass extends React.Component {
+  state = {count: 0};
+
+  onClick = () => {
+    this.setState({count: this.state.count + 1});
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="banner">
+          Total Count: {this.state.count} {this.state.count > 10 ? '🔥' : '🧊'}
+        </div>
+        <div className="counters">
+          <Counter total={this.state.count} onClick={this.onClick}/>
+          <Counter total={this.state.count} onClick={this.onClick}/>
+        </div>
+      </div>
+    );
+  }
+}

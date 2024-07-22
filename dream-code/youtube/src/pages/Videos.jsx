d996fa5312d60875112bee8ae4c9e0a749df3bc3
Eui Lee <lechan1317@gmail.com>
2024-07-22 17:31:29 +0900

    feat: mock data

--- dream-code/youtube/src/pages/Videos.jsx
+++ dream-code/youtube/src/pages/Videos.jsx
@@ -1,7 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Video {keyword ? `🔍${keyword}` : "🔥"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ["videos", keyword],
+    queryFn: async () => {
+      return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+        .then((res) => res.json())
+        .then((data) => data.items);
+    },
+  });
+
+  return (
+    <>
+      <div>Video {keyword ? `🔍${keyword}` : "🔥"}</div>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id.videoId} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }

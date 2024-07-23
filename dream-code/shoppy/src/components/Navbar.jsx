0210b41a2c9025a72c3c6d27154aafac306bf9ee
Eui Lee <lechan1317@gmail.com>
2024-07-23 16:48:33 +0900

    Feat: user profile

--- dream-code/shoppy/src/components/Navbar.jsx
+++ dream-code/shoppy/src/components/Navbar.jsx
@@ -3,21 +3,13 @@ import { BsFillPencilFill } from "react-icons/bs";
 import { FiShoppingBag } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { login, logout, onUserStateChange } from "../firebase/firebase";
+import User from "./User";
 
 export default function NavBar() {
   const [user, setUser] = useState();
   useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
+    onUserStateChange(setUser);
   }, []);
-  const handleLogin = () => {
-    login().then(setUser);
-  };
-  const handleLogout = () => {
-    logout().then(setUser);
-  };
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -30,8 +22,9 @@ export default function NavBar() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );

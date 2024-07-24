0853e5b5ba276a66ba78d1b8d80aa552f2d80451
Eui Lee <lechan1317@gmail.com>
2024-07-24 10:12:12 +0900

    feat: new product form

--- dream-code/shoppy/src/pages/NewProduct.jsx
+++ dream-code/shoppy/src/pages/NewProduct.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "../components/ui/Button";
 
 export default function NewProduct() {
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "file") {
+      setFile(files && files[0]);
+      return;
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      New Product
-    </div>
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt="product" />}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ""}
+          placeholder="products"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ""}
+          placeholder="price"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ""}
+          placeholder="category"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.description ?? ""}
+          placeholder="description"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="option"
+          value={product.options ?? ""}
+          placeholder="separator by comma"
+          required
+          onChange={handleChange}
+        />
+        <Button text="submit product" />
+      </form>
+    </section>
   );
 }
-

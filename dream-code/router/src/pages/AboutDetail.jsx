30ce6972a4842a0723893996c48c14c3e4c8ad59
Eui Lee <lechan1317@gmail.com>
2024-07-25 13:03:33 +0900

    Feat: change dir name

--- dream-code/router/src/pages/AboutDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function AboutDetail() {
-  const { aboutId } = useParams();
-  return <div>AboutDetail {aboutId}</div>;
-}

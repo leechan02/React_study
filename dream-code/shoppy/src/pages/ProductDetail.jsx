90a1ba1b1e6705a41d7cfce45fb9cf8ed9af7d54
Eui Lee <lechan1317@gmail.com>
2024-07-25 09:22:28 +0900

    feat: card detail

--- dream-code/shoppy/src/pages/ProductDetail.jsx
+++ dream-code/shoppy/src/pages/ProductDetail.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Button from "../components/ui/Button";
 
 export default function ProductDetail() {
+  const {
+    state: {
+      product: { image, title, category, price, options, description },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = (e) => setSelected(e.target.value);
+  const handleClick = (e) => {
+    console.log(`Added ${selected} of ${title} to cart`);
+  };
   return (
-    <div>
-      Product Detail
-    </div>
+    <>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <img src={image} alt={title} className="w-full px-4 basis-7/12" />
+        <div className="w-full basis-5/12 flex flex-col p-4">
+          <h2 className="text-3xl font-bold py-2">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-gray-400">
+            {price}
+          </p>
+          <p className="py-4 text-lg">{description}</p>
+          <div className="flex items-center">
+            <label className="text-brand font-bold" htmlFor="select">
+              option:
+            </label>
+            <select
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              id="select"
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((item, index) => (
+                  <option key={index}>{item}</option>
+                ))}
+            </select>
+            <Button text={`Add to Cart`} onClick={handleClick} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
-

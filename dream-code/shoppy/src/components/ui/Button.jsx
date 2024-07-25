22655cb6bc1e6685c9540b9cbb28676aa2614e90
Eui Lee <lechan1317@gmail.com>
2024-07-25 10:13:17 +0900

    feat: my cart ui

--- dream-code/shoppy/src/components/ui/Button.jsx
+++ dream-code/shoppy/src/components/ui/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ text, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110"
+      className="bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110 flex justify-center items-center cursor-pointer"
     >
       {text}
     </div>
--- dream-code/shoppy/src/pages/MyCart.jsx
+++ dream-code/shoppy/src/pages/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from "../components/CartItem";
 import PriceCard from "../components/PriceCard";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaEquals } from "react-icons/fa";
+import Button from "../components/ui/Button";
 
 const SHIPPING = 3000;
 export default function MyCart() {
@@ -24,23 +25,24 @@ export default function MyCart() {
     );
 
   return (
-    <section>
-      <p>My Cart</p>
+    <section className="p-8 flex flex-col">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">My Cart</p>
       {!hasProducts && <p>No products in cart</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
             {products.map((product) => (
               <CartItem key={product.id} product={product} uid={uid} />
             ))}
           </ul>
-          <div>
+          <div className="flex justify-between items-center mb-4 px-2 md:px-8 lg:px-16">
             <PriceCard text="Total Products" price={totalPrice} />
-            <BsFillPlusCircleFill />
+            <BsFillPlusCircleFill className="shrink-0" />
             <PriceCard text="Shipping Fee" price={SHIPPING} />
-            <FaEquals />
+            <FaEquals className="shrink-0" />
             <PriceCard text="Total" price={totalPrice + SHIPPING} />
           </div>
+          <Button text="Order" />
         </>
       )}
     </section>

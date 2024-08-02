1e89b396d340ca27f781bd769aba860d858dc11c
Eui Lee <lechan1317@gmail.com>
2024-08-02 16:13:07 +0900

    feat: movie

--- nomadcoder/practice/src/App.jsx
+++ nomadcoder/practice/src/App.jsx
@@ -1,27 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState();
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/coins")
-      .then((response) => response.json())
-      .then((data) => {
-        setCoins(data);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! ({coins?.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins?.map((coin, index) => (
-          <li key={index}>{coin.name} ({coin.symbol})</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return null;
 }
 
 export default App;

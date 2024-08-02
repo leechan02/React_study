855dd822e6c61c2825d5c6aca661c3409627292f
Eui Lee <lechan1317@gmail.com>
2024-08-02 14:35:36 +0900

    feat: useEffect

--- nomadcoder/introduction/src/App.js
+++ nomadcoder/introduction/src/App.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter(counter + 1);
-  console.log('render');
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("search for", keyword);
+  }, [keyword]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type='text' placeholder='la' />
       <h1>{counter}</h1>
       <button onClick={onClick}>Add 1</button>
     </div>

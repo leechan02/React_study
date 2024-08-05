f13ffae21d3eb36947c3ae936cb5970c7af6a547
Eui Lee <lechan1317@gmail.com>
2024-08-05 14:09:03 +0900

    feat: useTitle

--- nomadcoder/hooks/src/App.jsx
+++ nomadcoder/hooks/src/App.jsx
@@ -1,15 +1,6 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const sayHello = () => console.log("Hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
   return (
     <div className='App'>
-      <h1>Hi</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }

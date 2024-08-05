7aec66e92bcd9959795d5a9e7925e7659dc4cec1
Eui Lee <lechan1317@gmail.com>
2024-08-05 13:53:21 +0900

    Feat: useTabs

--- nomadcoder/hooks/src/App.jsx
+++ nomadcoder/hooks/src/App.jsx
@@ -1,12 +1,24 @@
-import useInput from './useInput';
+import useTabs from './useTabs';
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
 
 function App() {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className='App'>
-      <h1>Hello</h1>
-      <input placeholder='Name' {...name}/>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }

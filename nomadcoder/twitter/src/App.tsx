ef3eacfff527bddf8f80a54c564e7227087a7a4a
Eui Lee <lechan1317@gmail.com>
2024-08-06 10:13:21 +0900

    feat: create account

--- nomadcoder/twitter/src/App.tsx
+++ nomadcoder/twitter/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
@@ -18,6 +18,12 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
@@ -29,10 +35,10 @@ function App() {
     init();
   }, []);
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
--- nomadcoder/twitter/src/pages/CreateAccount.tsx
+++ nomadcoder/twitter/src/pages/CreateAccount.tsx
@@ -1,10 +1,120 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await updateProfile(credentials.user, { displayName: name });
+      navigate("/");
+    } catch (e) {
+    } finally {
+      setLoading(false);
+    }
+    console.log(name, email, password);
+  };
   return (
-    <div>
-      account
-    </div>
+    <Wrapper>
+      <Title>Join 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name='name'
+          value={name}
+          onChange={onChange}
+          placeholder='Name'
+          type='text'
+          required
+        />
+        <Input
+          name='email'
+          value={email}
+          onChange={onChange}
+          placeholder='Email'
+          type='email'
+          required
+        />
+        <Input
+          name='password'
+          value={password}
+          onChange={onChange}
+          placeholder='Password'
+          type='password'
+          required
+        />
+        <Input
+          type='submit'
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
   );
 }
-

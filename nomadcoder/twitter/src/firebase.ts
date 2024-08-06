94bc484812e58bdd305acbeaf322244d4572a473
Eui Lee <lechan1317@gmail.com>
2024-08-06 11:41:51 +0900

    feat: env update

--- nomadcoder/twitter/src/firebase.ts
+++ nomadcoder/twitter/src/firebase.ts
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYnOJ5QEaxtNGVs2x1FAzPzhhz9hHDObg",
-  authDomain: "ntwitter-b6537.firebaseapp.com",
-  projectId: "ntwitter-b6537",
-  storageBucket: "ntwitter-b6537.appspot.com",
-  messagingSenderId: "892067274256",
-  appId: "1:892067274256:web:5dffa077fbb1782d47b495"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);

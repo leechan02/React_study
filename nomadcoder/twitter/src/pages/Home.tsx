f7883d788cf8a6a73082b273ed1dc5b43cb102db
Eui Lee <lechan1317@gmail.com>
2024-08-08 13:27:42 +0900

    feat: navigation bar

--- nomadcoder/twitter/src/pages/Home.tsx
+++ nomadcoder/twitter/src/pages/Home.tsx
@@ -1,11 +1,6 @@
-import { auth } from '../firebase';
-
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  }
   return (
-    <h1><button onClick={logOut}>Log Out</button></h1>
+    <h1>home</h1>
   );
 }
 

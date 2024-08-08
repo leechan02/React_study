d3bce8903775a8fd9febbb1ba467e000a04f382c
Eui Lee <lechan1317@gmail.com>
2024-08-08 14:48:46 +0900

    feat: realtime

--- nomadcoder/twitter/src/components/Tweet.tsx
+++ nomadcoder/twitter/src/components/Tweet.tsx
@@ -10,7 +10,11 @@ const Wrapper = styled.div`
   border-radius: 15px;
 `;
 
-const Column = styled.div``;
+const Column = styled.div`
+  &:last-child {
+    place-self: end;
+  }
+`;
 
 const Photo = styled.img`
   width: 100px;
@@ -35,11 +39,7 @@ export default function Tweet({ username, photo, tweet }: ITweet) {
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
       </Column>
-      {photo ? (
-        <Column>
-          <Photo src={photo} />
-        </Column>
-      ) : null}
+      <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
   );
 }
